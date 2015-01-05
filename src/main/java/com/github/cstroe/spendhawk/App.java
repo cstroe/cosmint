d9a68964f30356398b0c1e2ebb12c3e1deba4ad1
Cosmin Stroe <cstroe@gmail.com>
2015-01-04 20:27:15 -0600

    Initial commit.

---
+++ src/main/java/com/github/cstroe/spendhawk/App.java
@@ -0,0 +1,18 @@
+package com.github.cstroe.spendhawk;
+
+import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.factory.GraphDatabaseFactory;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        GraphDatabaseService graphDb = new GraphDatabaseFactory().newEmbeddedDatabase("var/graphDb");
+        graphDb.shutdown();
+        System.out.println( "Hello World after Neo4J!" );
+    }
+}
