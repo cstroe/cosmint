e65020ae2eadfdfcfae2270386aa7fa642e26bac
Cosmin Stroe <cstroe@backstopsolutions.com>
2015-01-06 17:50:24 -0600

    Made tests more readable.

--- src/test/java/com/github/cstroe/spendhawk/entity/AccountTest.java
+++ src/test/java/com/github/cstroe/spendhawk/entity/AccountTest.java
@@ -1,90 +1,21 @@
 package com.github.cstroe.spendhawk.entity;
 
-import org.junit.Before;
 import org.junit.Test;
-
-import java.lang.reflect.Field;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.*;
 
 import static org.junit.Assert.assertEquals;
 
-public class AccountTest {
+public class AccountTest extends EntityTestSetup {
 
-    private List<Account> accountsList = new ArrayList<>();
-    private List<Transaction> transactionList = new ArrayList<>();
-
-    private String[] accountsTemplate = {
-            // Account ID, Account Name
-            "1", "Account1"
-    };
-
-    private String[] transactionsTemplate = {
-            // Transaction ID, Account ID, Amount, Date ("yyMMddHHmmss"), Description
-            "1", "1", "10.99", "010101000000", "Transaction 1",
-            "1", "1", "11.40", "010101000000", "Transaction 2"
-    };
-
-    private void createAccounts() {
-        try {
-            for (int i = 0; i < accountsTemplate.length; i += 2) {
-                Long accountId = Long.parseLong(accountsTemplate[i]);
-                String accountName = accountsTemplate[i+1];
-
-                // create account
-                Account account = new Account();
-
-                // set account properties
-                Field idField = account.getClass().getDeclaredField("id");
-                idField.setAccessible(true);
-                idField.set(account, accountId);
-                idField.setAccessible(false);
-                account.setName(accountName);
-                account.setTransactions(new ArrayList<>());
-                accountsList.add(account);
-            }
-        } catch(Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void createTransactions() {
-        try {
-            DateFormat dateFormat = new SimpleDateFormat("yyMMddHHmmss");
-            for (int i = 0; i < transactionsTemplate.length; i += 5) {
-                Long transactionId = Long.parseLong(transactionsTemplate[i]);
-                Long accountId = Long.parseLong(transactionsTemplate[i+1]);
-                Account account = accountsList.stream().filter(a->a.getId().equals(accountId)).findFirst().get();
-                Double amount = Double.parseDouble(transactionsTemplate[i+2]);
-                Date effectiveDate = dateFormat.parse(transactionsTemplate[i+3]);
-                String transactionName = transactionsTemplate[i+4];
-
-                // create transaction
-                Transaction transaction = new Transaction();
-
-                // set transaction properties
-                Field idField = transaction.getClass().getDeclaredField("id");
-                idField.setAccessible(true);
-                idField.set(transaction, transactionId);
-                idField.setAccessible(false);
-                transaction.setAccount(account);
-                transaction.setAmount(amount);
-                transaction.setEffectiveDate(effectiveDate);
-                transaction.setDescription(transactionName);
-                transaction.getAccount().getTransactions().add(transaction); // add the Transaction to the Account.
-
-                transactionList.add(transaction);
-            }
-        } catch(Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Before
-    public void setUp() {
-        createAccounts();
-        createTransactions();
+    {
+        accountsTemplate = new String[] {
+                // Account ID, Account Name
+                "1", "Account1"
+        };
+        transactionsTemplate = new String[] {
+                // Transaction ID, Account ID, Amount, Date ("yyMMddHHmmss"), Description
+                "1", "1", "10.99", "010101000000", "Transaction 1",
+                "1", "1", "11.40", "010101000000", "Transaction 2"
+        };
     }
 
     @Test
---
+++ src/test/java/com/github/cstroe/spendhawk/entity/EntityTestSetup.java
@@ -0,0 +1,81 @@
+package com.github.cstroe.spendhawk.entity;
+
+import org.junit.Before;
+
+import java.lang.reflect.Field;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class EntityTestSetup {
+
+    protected List<Account> accountsList = new ArrayList<>();
+    protected List<Transaction> transactionList = new ArrayList<>();
+
+    protected String[] accountsTemplate;
+    protected String[] transactionsTemplate;
+
+    @Before
+    public void setUp() {
+        createAccounts();
+        createTransactions();
+    }
+
+    private void createAccounts() {
+        try {
+            for (int i = 0; i < accountsTemplate.length; i += 2) {
+                Long accountId = Long.parseLong(accountsTemplate[i]);
+                String accountName = accountsTemplate[i+1];
+
+                // create account
+                Account account = new Account();
+
+                // set account properties
+                Field idField = account.getClass().getDeclaredField("id");
+                idField.setAccessible(true);
+                idField.set(account, accountId);
+                idField.setAccessible(false);
+                account.setName(accountName);
+                account.setTransactions(new ArrayList<>());
+                accountsList.add(account);
+            }
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void createTransactions() {
+        try {
+            DateFormat dateFormat = new SimpleDateFormat("yyMMddHHmmss");
+            for (int i = 0; i < transactionsTemplate.length; i += 5) {
+                Long transactionId = Long.parseLong(transactionsTemplate[i]);
+                Long accountId = Long.parseLong(transactionsTemplate[i+1]);
+                Account account = accountsList.stream().filter(a->a.getId().equals(accountId)).findFirst().get();
+                Double amount = Double.parseDouble(transactionsTemplate[i+2]);
+                Date effectiveDate = dateFormat.parse(transactionsTemplate[i+3]);
+                String transactionName = transactionsTemplate[i+4];
+
+                // create transaction
+                Transaction transaction = new Transaction();
+
+                // set transaction properties
+                Field idField = transaction.getClass().getDeclaredField("id");
+                idField.setAccessible(true);
+                idField.set(transaction, transactionId);
+                idField.setAccessible(false);
+                transaction.setAccount(account);
+                transaction.setAmount(amount);
+                transaction.setEffectiveDate(effectiveDate);
+                transaction.setDescription(transactionName);
+                transaction.getAccount().getTransactions().add(transaction); // add the Transaction to the Account.
+
+                transactionList.add(transaction);
+            }
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
