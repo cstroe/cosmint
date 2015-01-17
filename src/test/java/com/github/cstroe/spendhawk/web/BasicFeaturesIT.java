3f2839907bc26321e152d80199abf36e6b5ce9b5
Cosmin Stroe <cstroe@backstopsolutions.com>
2015-01-17 10:26:32 -0600

    Cleaner and more thorough tests.

--- src/test/java/com/github/cstroe/spendhawk/web/BasicFeaturesIT.java
+++ src/test/java/com/github/cstroe/spendhawk/web/BasicFeaturesIT.java
@@ -4,6 +4,7 @@ import com.github.cstroe.spendhawk.web.user.UserManagerServlet;
 import com.github.cstroe.spendhawk.web.user.UsersServlet;
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
 import org.jboss.arquillian.container.test.api.RunAsClient;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.arquillian.junit.InSequence;
@@ -19,14 +20,13 @@ import javax.servlet.http.HttpServlet;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.lang.reflect.Field;
+import java.net.MalformedURLException;
 import java.net.URL;
 
+import static com.github.cstroe.spendhawk.util.ServletUtil.servletPath;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThat;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 @RunWith(Arquillian.class)
 public class BasicFeaturesIT {
@@ -34,30 +34,27 @@ public class BasicFeaturesIT {
     @ArquillianResource
     URL deploymentUrl;
 
-    private static String userDetailUrl;
+    private static String userDetailPath;
+
+    private HttpResponse<String> response;
 
     @Test
     @RunAsClient
     @InSequence(100)
     public void connectToWelcomeServlet() throws Exception {
-        String url = deploymentUrl + WelcomeServlet.PATH.substring(1);
-        HttpResponse<String> response = Unirest.get(url).asString();
+        response = connect(WelcomeServlet.class);
         assertResponseStatus(200, response);
 
         Document doc = Jsoup.parse(response.getBody());
-        Elements links = doc.getElementsByTag("a");
-        String usersUrl = servletPath(deploymentUrl, UsersServlet.class);
-
-        assertEquals("A link from the welcome page to the users page must exist. Url: " + usersUrl,
-                findLink(usersUrl, links), true);
+        boolean usersLinkExists = findLink(fullServletPath(UsersServlet.class), doc);
+        assertEquals("A link from the welcome page to the users page must exist.", usersLinkExists, true);
     }
 
     @Test
     @RunAsClient
     @InSequence(200)
     public void connectToUsersServlet() throws Exception {
-        String url = deploymentUrl + UsersServlet.PATH.substring(1);
-        HttpResponse<String> response = Unirest.get(url).asString();
+        response = connect(UsersServlet.class);
         assertResponseStatus(200, response);
 
         Document doc = Jsoup.parse(response.getBody());
@@ -70,8 +67,7 @@ public class BasicFeaturesIT {
     @RunAsClient
     @InSequence(300)
     public void connectToUserManagerServlet() throws Exception {
-        String url = deploymentUrl + UserManagerServlet.PATH.substring(1);
-        HttpResponse<String> response = Unirest.get(url).asString();
+        response = connect(UserManagerServlet.class);
         assertResponseStatus(200, response);
     }
 
@@ -79,17 +75,19 @@ public class BasicFeaturesIT {
     @RunAsClient
     @InSequence(400)
     public void createUser() throws Exception {
-        String url = deploymentUrl + UserManagerServlet.PATH.substring(1);
-        HttpResponse<String> response = Unirest.post(url)
+        HttpResponse<String> response = Unirest.post(url(UserManagerServlet.class))
             .field("user.name", "testuser")
             .field("action", "Add User")
             .asString();
         assertResponseStatus(302, response);
 
-        url = deploymentUrl + UsersServlet.PATH.substring(1);
-        response = Unirest.get(url).asString();
+        // location header takes you to accounts page for that user
+        String redirectUrl = response.getHeaders().getFirst("location");
+        String urlPath = new URL(redirectUrl).getPath();
+        assertTrue(urlPath.startsWith(fullServletPath(AccountsServlet.class)));
 
-        assertResponseStatus(200, response);
+        String viewUsersUrl = url(UsersServlet.class);
+        response = Unirest.get(viewUsersUrl).asString();
 
         Document doc = Jsoup.parse(response.getBody());
         Elements links = doc.getElementsByClass("userLink");
@@ -97,45 +95,101 @@ public class BasicFeaturesIT {
         assertThat(links.size(), is(equalTo(1)));
 
         Element link = links.get(0);
-        userDetailUrl = link.attr("href");
+        userDetailPath = link.attr("href");
 
-        assertTrue(userDetailUrl.contains(AccountsServlet.PATH + "?"));
+        assertTrue(userDetailPath.startsWith(fullServletPath(AccountsServlet.class) + "?"));
     }
 
     @Test
     @RunAsClient
     @InSequence(500)
     public void viewAccounts() throws Exception {
-        String url = deploymentUrl + userDetailUrl;
-        HttpResponse<String> response = Unirest.get(url).asString();
+        response = connect(userDetailPath);
         assertResponseStatus(200, response);
+
+        Document doc = Jsoup.parse(response.getBody());
+        boolean accountManagerLinkExists = findLink(fullServletPath(AccountManagerServlet.class), doc);
+        assertEquals("A link from the accounts page to the account manager page must exist.", accountManagerLinkExists, true);
     }
 
     /**
-     * Build the path of a given servlet, to be used when verifying that links
-     * exist.
+     * Add the deployment context path to a servlet's path to create an absolute path.
+     *
+     * For example, given:
+     *    deploymentUrl: "http://127.0.0.1:8080/a1dgfdw"
+     *    servlet path: "/accounts/manage"
+     *
+     * Return: "/a1dgfdw/accounts/manage"
      */
-    private static String servletPath(URL deploymentUrl, Class<? extends HttpServlet> servlet) {
+    private String fullServletPath(Class<? extends HttpServlet> servlet) {
         StringBuilder servletUrl = new StringBuilder();
-        try {
-            Field path = servlet.getDeclaredField("PATH");
-            String servletPath = (String) path.get(servlet);
-            String deploymentPath = deploymentUrl.getPath();
-            servletUrl.append(deploymentPath.substring(0, deploymentPath.length()-1))
-                      .append(servletPath);
+        final String deploymentPath = deploymentUrl.getPath();
+        servletUrl.append(deploymentPath.substring(0, deploymentPath.length()-1));
+        servletUrl.append(servletPath(servlet));
+        return servletUrl.toString();
+    }
 
-            return servletUrl.toString();
-        } catch(IllegalAccessException | NoSuchFieldException ex) {
+    /**
+     * A helper method to connect to a specific servlet on the test deployment.
+     */
+    private HttpResponse<String> connect(Class<? extends HttpServlet> servlet) {
+        try {
+            return Unirest.get(url(servlet)).asString();
+        } catch (UnirestException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private HttpResponse<String> connect(String absolutePath) {
+        try {
+            return Unirest.get(url(absolutePath)).asString();
+        } catch (UnirestException | MalformedURLException e) {
+            e.printStackTrace();
             return null;
         }
     }
 
     /**
+     * Build the deployment url given the request path on the server.
+     */
+    private String url(String requestPath) throws MalformedURLException {
+        StringBuilder url = new StringBuilder();
+        url.append("http://")
+           .append(deploymentUrl.getHost())
+           .append(":")
+           .append(deploymentUrl.getPort());
+
+        if(requestPath.startsWith(deploymentUrl.getPath())) {
+            url.append(requestPath);
+        } else {
+            url.append(deploymentUrl.getPath());
+            if(requestPath.startsWith("/")) {
+                url.append(requestPath.substring(1));
+            }
+            url.append(requestPath);
+        }
+
+        return url.toString();
+    }
+
+    /**
+     * Create the url for a servlet on the test deployment.
+     */
+    private String url(Class<? extends HttpServlet> servlet) {
+        return "http://" +
+               deploymentUrl.getHost() +
+               ":" +
+               deploymentUrl.getPort() +
+               fullServletPath(servlet);
+    }
+
+    /**
      * Find a link to the given path.
-     * @param links A list of links.
      * @return true if the path was found as one of the links' href, false if none was found
      */
-    private static boolean findLink(String path, Elements links) {
+    private static boolean findLink(String path, Document doc) {
+        Elements links = doc.getElementsByTag("a");
         if(path == null || links == null) {
             return false;
         }
@@ -147,6 +201,19 @@ public class BasicFeaturesIT {
                 linkFound = true;
             }
         }
+
+        if(!linkFound) {
+            try {
+                File tempFile = File.createTempFile("integrationTest", ".html");
+                FileWriter writer = new FileWriter(tempFile);
+                writer.write(doc.toString());
+                writer.close();
+                System.out.println("Saved response page to: file://" + tempFile.getAbsolutePath());
+            } catch(Exception ex) {
+                ex.printStackTrace();
+            }
+        }
+
         return linkFound;
     }
 
@@ -160,7 +227,7 @@ public class BasicFeaturesIT {
             FileWriter writer = new FileWriter(tempFile);
             writer.write(response.getBody());
             writer.close();
-            System.out.println("Saved error output to: file://" + tempFile.getAbsolutePath());
+            System.out.println("Saved response page to: file://" + tempFile.getAbsolutePath());
         }
 
         assertEquals(expected, response.getStatus());
