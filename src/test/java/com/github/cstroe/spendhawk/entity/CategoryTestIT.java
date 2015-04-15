941f82b5936a32010098772d6efff04d40d0a8f2
Cosmin Stroe <cstroe@backstopsolutions.com>
2015-04-15 02:02:33 -0500

    While we're at it, make sure the children are correctly persisted.

--- src/test/java/com/github/cstroe/spendhawk/entity/CategoryTestIT.java
+++ src/test/java/com/github/cstroe/spendhawk/entity/CategoryTestIT.java
@@ -6,10 +6,13 @@ import org.jboss.arquillian.junit.Arquillian;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.util.Set;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.not;
 import static org.hamcrest.Matchers.nullValue;
+import static org.junit.Assert.assertTrue;
 
 @RunWith(Arquillian.class)
 public class CategoryTestIT extends BaseIT {
@@ -23,6 +26,31 @@ public class CategoryTestIT extends BaseIT {
             .orElseThrow(Exceptions::categoryNotFound);
 
         assertThat(c1.getParent(), is(not(nullValue())));
+
+        commitTransaction();
+    }
+
+    @Test
+    public void testChildrenArePersisted() {
+        startTransaction();
+        User currentUser = User.findById(3l).orElseThrow(Exceptions::userNotFound);
+
+        Category p1 = Category.findById(currentUser, 10l)
+                .orElseThrow(Exceptions::categoryNotFound);
+
+        assertThat(p1.getChildren(), is(not(nullValue())));
+
+        Set<Category> children = p1.getChildren();
+        assertThat(children.size(), is(2));
+
+        Category c1 = Category.findById(currentUser, 11l)
+                .orElseThrow(Exceptions::categoryNotFound);
+
+        Category c2 = Category.findById(currentUser, 12l)
+                .orElseThrow(Exceptions::categoryNotFound);
+
+        assertTrue(children.contains(c1));
+        assertTrue(children.contains(c2));
 
         commitTransaction();
     }
