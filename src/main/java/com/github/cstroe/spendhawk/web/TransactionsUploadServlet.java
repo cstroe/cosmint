e22d9350c273168d1b50e1d32bd2ed8028546184
Cosmin Stroe <cstroe@backstopsolutions.com>
2015-06-03 09:03:21 -0500

    Create accounts if they don't exist.

--- src/main/java/com/github/cstroe/spendhawk/web/TransactionsUploadServlet.java
+++ src/main/java/com/github/cstroe/spendhawk/web/TransactionsUploadServlet.java
@@ -1,7 +1,11 @@
 package com.github.cstroe.spendhawk.web;
 
+import com.github.cstroe.spendhawk.bean.DateBean;
+import com.github.cstroe.spendhawk.bean.transaction.ChaseCSVParser;
 import com.github.cstroe.spendhawk.entity.Account;
-import com.github.cstroe.spendhawk.helper.TransactionsHelper;
+import com.github.cstroe.spendhawk.entity.CashFlow;
+import com.github.cstroe.spendhawk.entity.Transaction;
+import com.github.cstroe.spendhawk.entity.User;
 import com.github.cstroe.spendhawk.util.Ex;
 import com.github.cstroe.spendhawk.util.HibernateUtil;
 
@@ -14,6 +18,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -48,7 +53,7 @@ public class TransactionsUploadServlet extends HttpServlet {
         final String fileFormat = request.getParameter("format");
         final Part filePart = request.getPart("file");
         final Long accountId = Long.parseLong(request.getParameter("id"));
-        final boolean duplicateCheck = request.getParameter("duplicate_check") != null;
+        //final boolean duplicateCheck = request.getParameter("duplicate_check") != null;
 
         try {
             HibernateUtil.getSessionFactory().getCurrentSession().beginTransaction();
@@ -56,12 +61,38 @@ public class TransactionsUploadServlet extends HttpServlet {
             Account account = Account.findById(accountId)
                 .orElseThrow(Ex::accountNotFound);
 
-            List<String> messages = TransactionsHelper.processFile(filecontent, account, duplicateCheck, fileFormat);
+            Account incomeAccount = account.getUser().getDefaultIncomeAccount()
+                .orElseGet(() -> {
+                    Account ic = new Account();
+                    ic.setName(User.DEFAULT_INCOME_ACCOUNT_NAME);
+                    ic.setUser(account.getUser());
+                    ic.save();
+                    return ic;
+                });
 
-            messages.add("Duplicate check: " + Boolean.toString(duplicateCheck));
+            Account expenseAccount = account.getUser().getDefaultExpenseAccount()
+                .orElseGet(() -> {
+                    Account ec = new Account();
+                    ec.setName(User.DEFAULT_EXPENSE_ACCOUNT_NAME);
+                    ec.setUser(account.getUser());
+                    ec.save();
+                    return ec;
+                });
+
+            if("chase".equals(fileFormat)) {
+                DateBean dateBean = new DateBean();
+                ChaseCSVParser parser = new ChaseCSVParser(dateBean);
+                List<Transaction> transactions = parser.parse(filecontent, account, incomeAccount, expenseAccount);
+                for(Transaction t : transactions) {
+                    t.save();
+                    for(CashFlow f : t.getCashFlows()) {
+                        f.save();
+                    }
+                }
+            }
 
             request.setAttribute("account", account);
-            request.setAttribute("messages", messages);
+            request.setAttribute("messages", new ArrayList<String>());
             request.getRequestDispatcher(TEMPLATE).forward(request, response);
             HibernateUtil.getSessionFactory().getCurrentSession().getTransaction().commit();
         } catch (Exception ex) {
