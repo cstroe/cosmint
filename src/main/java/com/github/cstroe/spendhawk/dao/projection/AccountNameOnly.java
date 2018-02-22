955f3d7440133567f71328f35d79193a8ee95e53
Cosmin Stroe <cosmin@mailbox.org>
2018-02-22 07:58:56 -0600

    Add method to retrieve the account names only.

---
+++ src/main/java/com/github/cstroe/spendhawk/dao/projection/AccountNameOnly.java
@@ -0,0 +1,9 @@
+package com.github.cstroe.spendhawk.dao.projection;
+
+import lombok.Value;
+
+@Value
+public class AccountNameOnly {
+    private Long id;
+    private String name;
+}
--- src/main/java/com/github/cstroe/spendhawk/repository/AccountRepository.java
+++ src/main/java/com/github/cstroe/spendhawk/repository/AccountRepository.java
@@ -1,6 +1,7 @@
 package com.github.cstroe.spendhawk.repository;
 
 import com.github.cstroe.spendhawk.dao.AccountDao;
+import com.github.cstroe.spendhawk.dao.projection.AccountNameOnly;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.Collection;
@@ -9,6 +10,7 @@ import java.util.Optional;
 public interface AccountRepository extends CrudRepository<AccountDao, Integer> {
     Optional<AccountDao> findByIdAndUserId(Long accountId, Long userId);
     Optional<AccountDao> findByName(String name);
+    Collection<AccountNameOnly> findByUserId(Long userId);
 
     <T> Collection<T> findByUserId(Long userId, Class<T> type);
 }
